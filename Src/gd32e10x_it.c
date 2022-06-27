104b9b32d93f08e667a6b7e52188ca8e7d861413
Andrew <33828830+ADElectronics@users.noreply.github.com>
2022-06-27 18:43:31 +0300

    Update gd32e10x_it.c

--- Src/gd32e10x_it.c
+++ Src/gd32e10x_it.c
@@ -1,35 +1,7 @@
 #include "gd32e10x_it.h"
 #include "main.h"
 #include "systick.h"
-/*
-extern can_receive_message_struct g_receive_message;
-extern FlagStatus can0_receive_flag;
-extern FlagStatus can1_receive_flag;
 
-void CAN0_RX0_IRQHandler(void)
-{
-   can_message_receive(CAN0, CAN_FIFO0, &g_receive_message);
-   can0_receive_flag = SET;
-}
-
-void CAN1_RX0_IRQHandler(void)
-{
-   can_message_receive(CAN1, CAN_FIFO0, &g_receive_message);
-   can1_receive_flag = SET;
-}
-
-void CAN0_RX1_IRQHandler(void)
-{
-   can_message_receive(CAN0, CAN_FIFO1, &g_receive_message);
-   can0_receive_flag = SET;
-}
-
-void CAN1_RX1_IRQHandler(void)
-{
-   can_message_receive(CAN1, CAN_FIFO1, &g_receive_message);
-   can1_receive_flag = SET;
-}
-*/
 void NMI_Handler(void)
 {
 }
